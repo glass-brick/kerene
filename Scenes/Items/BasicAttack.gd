6b2f36cfe5ef0859badd5885681a75d998e60809
GelosoFederico <gelosofederico@gmail.com>
2021-04-25 00:35:26 -0300

    Ahora el bassic attack tambien anda

--- Scenes/Items/BasicAttack.gd
+++ Scenes/Items/BasicAttack.gd
@@ -3,4 +3,15 @@ var item_name = "BasicAttack"
 var item_owner = false
 
 func use():
-	print("uso y todo bien")
+	# Copy paste furioso del codigo en cursor
+	var target = item_owner.get_global_mouse_position()
+	var space_state = item_owner.get_world_2d().direct_space_state
+
+	var intersections = space_state.intersect_point(target, 1, [item_owner, item_owner.tilemap])
+	if not intersections.empty():
+		var target_thing = intersections[0].collider
+
+		if target_thing.collision_layer == 2:
+			if target_thing.has_method('_on_hit'):
+				target_thing.call('_on_hit', item_owner.damage)
+

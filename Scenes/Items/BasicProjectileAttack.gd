940c62af8f2ddb0c3185f5f30c0d921f55c8fa5b
GelosoFederico <gelosofederico@gmail.com>
2021-04-25 12:55:53 -0300

    Projectiles del jugador ahora toman el daño correcto

--- Scenes/Items/BasicProjectileAttack.gd
+++ Scenes/Items/BasicProjectileAttack.gd
@@ -19,9 +19,9 @@ func use():
 
 func fire_projectile_in_dir(direction):
 	var projectile = projectileBase.instance()
-	projectile.speed = projectile_speed
-	projectile.damage = projectile_damage
-	projectile.projectile_range = projectile_range
+	# projectile.speed = projectile_speed
+	# projectile.damage = projectile_damage
+	# projectile.projectile_range = projectile_range
 	projectile.direction = direction
 	item_owner.get_tree().get_root().get_node("Level").add_child(projectile)
 	projectile.position = item_owner.position
